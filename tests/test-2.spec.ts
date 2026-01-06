234c6d84c80bec242931db0345040351864167f0
Huyenltn <ngochuyen121212@gmail.com>
2026-01-06 13:58:58 +0700

    Update code at Register account testcase

--- tests/test-2.spec.ts
+++ tests/test-2.spec.ts
@@ -13,30 +13,36 @@ test('Register account', async ({ page }) => {
     await expect(page.getByText('New user signup')).toBeVisible();
     // Enter name and email address
     await page.getByPlaceholder('Name').fill('Huyenltn');
-    await page.locator('[data-qa="signup-email"]').fill('Huyenltn3@gmail.com');
+    await page.locator('[data-qa="signup-email"]').fill('Huyenltn5@gmail.com');
     // Click 'Signup' button 
     await page.locator('[data-qa="signup-button"]').click();
     // Verify Enter Account Information is displayed
     await expect(page.getByText('Enter Account Information')).toBeVisible();
-
-    await page.click('label:has-text("Mrs")');
-    await page.fill('input[name="password"]', 'Huyenltn');
-    await page.selectOption('select#days', '12');
-    await page.selectOption('select[name="months"]', '12');
-    await page.selectOption('select[name="years"]', '1992');
-    await page.click('label:has-text("Sign up for our newsletter")');
-    await page.click('label:has-text("Receive special offers from our partners")');
-    await page.fill('input[id="first_name"]', 'Huyen');
-    await page.fill('input[id="last_name"]', 'Le');
-    await page.fill('input[id="company"]', 'ABC');
-    await page.fill('input[id="address1"]', 'HCM');
-    await page.selectOption('select[name="country"]', { label: 'Canada' });
-    await page.fill('input[id="state"]', 'Q7');
-    await page.fill('input[id="city"]', 'HCM');
-    await page.fill('input[id="zipcode"]', '3134');
-    await page.fill('input[id="mobile_number"]', '0359317942');
-    //click 'Create Account' button
-    await page.click('button[type="submit"]');
+    // Click title 'Mrs'
+    await page.locator('[for="id_gender2"]').click();
+    //Fill Password
+    await page.locator('[data-qa="password"]').fill('Huyenltn');
+    // Select Date of birth: Day, Month, Year
+    await page.locator('[data-qa="days"]').selectOption('12');
+    await page.locator('[data-qa="months"]').selectOption('12');
+    await page.locator('[data-qa="years"]').selectOption('1999');
+    // Select checkbox 'Sign up for our newsletter!'
+    await page.getByLabel('Sign up for our newsletter!').check();
+    // Select checkbox 'Receive special offers from our partners!'
+    await page.getByLabel('Receive special offers from our partners!').check();
+    // Fill details: First name, Last name, Company, Address, Address2, Country, State, City, Zipcode, Mobile Number
+    await page.locator('[data-qa="first_name"]').fill('Huyen');
+    await page.locator('[data-qa="last_name"]').fill('Le');
+    await page.locator('[data-qa="company"]').fill('ABC');
+    await page.locator('[data-qa="address"]').fill('HCM');
+    await page.locator('[data-qa="address2"]').fill('HCM');
+    await page.locator('[data-qa="country"]').selectOption('Canada');
+    await page.locator('[data-qa="state"]').fill('Q7');
+    await page.locator('[data-qa="city"]').fill('HCM');
+    await page.locator('[data-qa="zipcode"]').fill('3134');
+    await page.locator('[data-qa="mobile_number"]').fill('0359317942');
+    // Click 'Create Account' button
+    await page.locator('[data-qa="create-account"]').click();
     //Account Created!
     await expect(page.getByText('ACCOUNT CREATED!')).toBeVisible();
     //click 'Continue' button
